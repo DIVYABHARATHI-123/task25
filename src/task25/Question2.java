3e9308fdea0ca89870543cb670c8f9ccc98be4da
DIVYABHARATHI-123 <dbharathi11997@gmail.com>
2024-10-29 02:22:40 -0700

    First commit

--- src/task25/Question2.java
+++ src/task25/Question2.java
@@ -1,4 +1,4 @@
-package task24;
+package task25;
 
 import java.time.Duration;
 
@@ -6,26 +6,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
-public class Locators {
+public class Question2 {
 
 	public static void main(String[] args) {
-		
-		WebDriver driver=new ChromeDriver();
+	
+        WebDriver driver=new ChromeDriver();
 		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://www.guvi.in/register");
+		driver.get("https://www.guvi.in/");
 		driver.manage().window().maximize();
 		
-		driver.findElement(By.id("name")).sendKeys("Divyabharathi");
+		driver.findElement(By.className("bg-green-500 hover:bg-green-600 text-white font-normal py-2 px-4 rounded text-base min-h-8 h-8 align-middle mr-2")).click();
 		driver.findElement(By.id("email")).sendKeys("vishalpm000@gmail.com");
 		driver.findElement(By.id("password")).sendKeys("Ziya@1000");
-		driver.findElement(By.id("mobileNumber")).sendKeys("8124394266");
+		driver.findElement(By.id("mobileNumber")).sendKeys("7845994236");
 		driver.findElement(By.className("signup-btn")).click();
 		
 		String s = driver.getTitle();
 		System.out.println(s);
-        
+		
+		//close the browser
+		  driver.quit();
 	}
 
 }
